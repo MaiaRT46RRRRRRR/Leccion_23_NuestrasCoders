c058af96d309dd8b2aa3bfa60973460c9c8440af
Maia <maia.rt.46@gmail.com>
2017-04-19 12:57:45 -0500

    Prueba

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//Trayendonos los elementos que utilizaremos
+var todo = document.getElementById("todo");
+var h1 = document.createElement("h1");
+var texto = document.createTextNode('Nuestras Coders');
+    h1.appendChild(texto);
+    todo.appendChild(h1);
+
+var hr = document.createElement("hr");
+	todo.appendChild(hr);
+var coders=["valentina Peña" ,"Carola Carrillo","Aitana Vega","Tatiana Martiz","Silvana Rosas","Abril Bilbao"]
+
